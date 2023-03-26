7b1df78bffc12b0a39ed16c9dabe31dee435abbe
shayCamp <76885270+ssutl@users.noreply.github.com>
2023-03-26 07:56:26 +0100

    Modal Transcripts 🔔

--- src/Components/summaryModal.tsx
+++ src/Components/summaryModal.tsx
@@ -1,10 +1,47 @@
+import { summaryArrayType } from "@/pages/api/summary";
 import React, { useState, useEffect } from "react";
 import styles from "../styles/summaryModal.module.scss";
 
-//interface summaryModalProps {}
+type SummaryModalProps = {
+  close: () => void;
+};
+const SummaryModal = ({ close }: SummaryModalProps) => {
+  const [sessionSummaries, setSessionSummaries] = useState<summaryArrayType[]>(
+    []
+  );
+  useEffect(() => {
+    const summaries = sessionStorage.getItem("logged_summaries");
+    console.log("summaries", summaries);
+    if (summaries) {
+      const parsedArray = JSON.parse(summaries);
+      setSessionSummaries(parsedArray);
+    }
+  }, []);
 
-const summaryModal = () => {
-  return <div className={styles.modalOverlay}>summaryModal</div>;
+  return (
+    <div
+      className={styles.modalOverlay}
+      onClick={() => {
+        close();
+      }}
+    >
+      <div className={styles.modal}>
+        {sessionSummaries.length === 0 ? (
+          <p>No summaries this session</p>
+        ) : null}
+        <div className={styles.modalWidthContainer}>
+          <h1>Transcripts</h1>
+
+          {sessionSummaries.map((eachSummary) => (
+            <div>
+              <h1>{eachSummary.title}</h1>
+              <p>{eachSummary.summary}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default summaryModal;
+export default SummaryModal;
