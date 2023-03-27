c799c3d7890fcf23d3e0104f18e490b4feec093e
shayCamp <76885270+ssutl@users.noreply.github.com>
2023-03-27 01:05:30 +0100

    Fixing Header 🛠️

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,6 @@ import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta
-          name="Lecture Summaries"
-          content="Get summaries of your lectures within seconds"
-        />
-        <meta name="keywords" content="Video Summary" />
-        <title>LecSum</title>
-        <link rel="icon" href="/Resources/Logo.png" />
-      </Head>
       <Component {...pageProps} />
     </>
   );
